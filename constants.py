237328d9d0403953051c28572d3927eddb200dc9
naikdij88 <eugeny-nikolaeff@mail.ru>
2017-03-02 02:33:58 +0700

    publische

---
+++ constants.py
@@ -0,0 +1,2 @@
+class Data():
+    token = '342349274:AAFddwHfZiB-gAUANSJAnRu4iGIRWxFE-4k'
---
+++ main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import telebot
+from constants import Data 
+
+def main():
+    bot = telebot.TeleBot(Data.token)
+
+    @bot.message_handler(content_types=['text'])
+    def send_welcome(message):
+        if message.text == 'a':
+            bot.reply_to(message, "b")
+
+        elif message.text == 'b':
+            bot.reply_to(message, "a")
+    
+
+    bot.polling(none_stop=True, interval=0)
+
+
+if __name__ == '__main__':  
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit()
